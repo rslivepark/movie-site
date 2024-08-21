5171337b33282dfae1e81df65f857f38816d94e9
rslivepark <rslivepark@gmail.com>
2024-08-21 11:53:35 +0900

    1. router, pages 설정

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from './components/banner/Banner';
+
+export default function Homepage() {
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+}
+
+//1. 배너만들기 => popular movie[0] 보여주기
+//2. popular movie
+//3. toprated movie
+//4. upcoming movie
